91438ad7e861fe3676bad3500c7c905833ef7326
Graham Pagan <kkb14164@uni.strath.ac.uk>
2017-03-20 21:39:16 +0000

    Checks if input is numeric.

--- src/controller/RunListener.java
+++ src/controller/RunListener.java
@@ -214,17 +214,27 @@ public class RunListener implements ActionListener {
 				break;
 			
 			case "Apply Gravity: ":
+				try {
 				double grav = Double.parseDouble(gravity.getText());				
 				model.setGravity(grav);
 				System.out.println("Gravity set to: " + grav);
+				} catch (NumberFormatException gravityNumException){
+					JOptionPane.showMessageDialog(frame,
+						    "Gravity must be a numeric value.");
+				}
 				break;
 				
 			case "Apply Friction: ":
+				try{
 				double xFr = Double.parseDouble(xFric.getText());
 				double yFr = Double.parseDouble(yFric.getText());
 				model.setMu(xFr);
 				model.setMu2(yFr);
 				System.out.println("Friction set to Mu: " + xFr + " Mu2: " + yFr);
+				} catch (NumberFormatException fricNumException){
+					JOptionPane.showMessageDialog(frame,
+						    "Friction must be a numeric value.");
+				}
 				break;
 			case "Build Mode":
 				timer.stop();
