1d280bef8f5ab5573af27802f936bf13d1dfead4
rossc123 <rossc123@live.com>
2017-03-20 21:30:07 +0000

    made flipper into internal class of board. Might work, might've made things worse

--- src/view/Board.java
+++ src/view/Board.java
@@ -6,6 +6,8 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Stroke;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -25,13 +27,144 @@ import model.circle;
 import physics.Circle;
 import model.Line;
 
+import java.awt.Color;
+//import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.geom.AffineTransform;
 
-public  class Board extends JPanel implements Observer {
+
+
+public class Board extends JPanel implements Observer {
+
+	public class Flipper implements KeyListener{
+		
+		public Flipper(int x,int y){
+		xpos = x;
+		ypos = y;
+	//	setPreferredSize(new Dimension(800, 600));
+	//	setFocusable(true);
+
+		init();
+		updateTransforms();
+		//ID = id;
+		
+	}
+	
+	public void init() {
+		rect = new Rectangle(30, 100);
+		transform = new AffineTransform();
+		rotationTransform = new AffineTransform();
+		translateTransform = new AffineTransform();
+		addKeyListener(this);
+		
+		
+	}
+
+	public void paintFlipper(Graphics g) {
+
+		paintFlipper(g);
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setTransform(transform);
+		updateTransforms();
+		g2d.setColor(Color.RED);
+		g2d.fill(rect);
+		
+	}
+
+	public void keyPressed(KeyEvent k) {
+		int code=k.getKeyCode();
+		if(code== KeyEvent.VK_UP){
+			if (rotation == 0) {
+				rotation = rotation + 90;
+				repaint();
+			}
+		}
+		updateTransforms();
+	}
+
+	public void updateTransforms() {
+		// Resets transform to rotation
+		rotationTransform.setToRotation(Math.toRadians(rotation));
+		translateTransform.setToTranslation(getWidth() / 2, getHeight() / 2);
+
+		// Chain the transforms (Note order matters)
+		transform.setToIdentity();
+		transform.concatenate(translateTransform);
+		transform.concatenate(rotationTransform);
+	}
+
+	public void keyReleased(KeyEvent k) {
+		int code=k.getKeyCode();
+		if(code== KeyEvent.VK_UP){
+			rotation = rotation - 90;
+
+			repaint();
+
+		}
+		updateTransforms();
+
+	}
+
+	public void keyTyped(KeyEvent k) {
+	}
+
+	public String getID() {
+		// TODO Auto-generated method stub
+		return ID;
+	}
+	
+	public String getConnected(){
+		return connectedID;
+	}
+	
+	public void setConnected(String a){
+		connectedID = a;
+	}
+	
+	public void clearConnected(){
+		connectedID = "";
+	}
+	
+	public boolean isConnected(){
+		String clear = "";
+		
+		if(connectedID.equals(clear)){
+			return false;
+		}else{
+			return true;
+		}
+	}
+
+
+	
+	
+	
+	
+	}
 
 	private static final long serialVersionUID = 1L;
 	protected int width;
 	protected int height;
 	protected Model gm;
+	
+	private Rectangle rect;
+	private String ID;
+	private String connectedID;
+	private int xpos;
+	private int ypos;
+
+	private int xboardpos;
+	private int yboardpos;
+
+	private int rotation = 0;
+	AffineTransform transform;
+	AffineTransform rotationTransform;
+	AffineTransform translateTransform;
+	
 
 	public Board(int w, int h, Model m) {
 		// Observe changes in Model
@@ -127,6 +260,12 @@ public  class Board extends JPanel implements Observer {
 	public void update(Observable arg0, Object arg1) {
 			repaint();
 		}
+	
+	
+	
+	
+
+	
 	
 
 	
